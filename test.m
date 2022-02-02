cc90488605d875c685b784faa1834f5294449091
Srishti Patkar <56215409+srishtipatkar@users.noreply.github.com>
2022-02-02 23:37:04 +0530

    Update test.m

--- test.m
+++ test.m
@@ -1,11 +1,5 @@
-data=fopen("ch0_k-6_1.bin",'rb');
-noisySpeech=fread(data,'int16');
-noisySpeech=noisySpeech./32767;
-noisySpeech=noisySpeech(1:16000*20);
-% [noisySpeech,fs] = audioread('train-clean-100\train-clean-100\4195\186238\4195-186238-0003.flac');
-% [noisySpeech,fs] = audioread('google_speech\test\down\0f250098_nohash_0.wav');
-% [noisySpeech,fs] = audioread('alex_noisy.wav');
-% noisySpeech=resample(noisySpeech,16000,fs);
+[noisySpeech,fs] = audioread('noisy_speech');
+noisySpeech=resample(noisySpeech,16000,fs);
 features = extract(afe,noisySpeech);
 features = (features - mean(features,1)) ./ std(features,[],1);
 features = features';
